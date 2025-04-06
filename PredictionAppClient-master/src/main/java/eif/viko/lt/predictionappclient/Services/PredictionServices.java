4e47feba5476e9890d39f718d71c3a443a20e904
RubinoPD <robertaskask@gmail.com>
2025-04-06 10:32:07 +0300

    Added PredictionService interface to Services

---
+++ PredictionAppClient-master/src/main/java/eif/viko/lt/predictionappclient/Services/PredictionServices.java
@@ -0,0 +1,28 @@
+package eif.viko.lt.predictionappclient.Services;
+
+import eif.viko.lt.predictionappclient.Dto.GradeHistory;
+import eif.viko.lt.predictionappclient.Dto.PredictionRequest;
+import eif.viko.lt.predictionappclient.Dto.PredictionResponse;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PredictionServices {
+    @POST("api/predict/grade")
+    Call<PredictionResponse> predictGrade(@Body PredictionRequest request);
+
+    @GET("api/predict/grade")
+    Call<List<GradeHistory>> getPredictionHistory();
+
+    @GET("api/predict/analytics/weak-students")
+    Call<List<GradeHistory>> getWeakStudents();
+
+    @GET("api/predict/analytics/accuracy")
+    Call<Map<String, Object>> getPredictionAccuracy();
+
+    @PUT("api/predict/history/{id}/actual-grade")
+    Call<GradeHistory> updateActualGrade(@Path("id") Long id, @Body Map<String, String> actualGrade);
+
+}
